6a6fd5eadce530e25bd6cfc363d7777327628100
GrantPerkins <gcperk20@gmail.com>
2020-12-10 20:04:11 -0500

    Construct arm

--- DemoFIRST/src/main/java/frc/robot/RobotContainer.java
+++ DemoFIRST/src/main/java/frc/robot/RobotContainer.java
@@ -9,10 +9,12 @@ package frc.robot;
 
 import frc.robot.commands.chassis.DefaultDriveCommand;
 import frc.robot.input.Extreme3DPro;
+import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ChassisSubsystem;
 
 public class RobotContainer {
     public static final ChassisSubsystem chassisSubsystem = new ChassisSubsystem();
+    public static final ArmSubsystem armSubsystem = new ArmSubsystem();
     public static final Extreme3DPro joystick = new Extreme3DPro(0);
     public RobotContainer() {
         chassisSubsystem.setDefaultCommand(new DefaultDriveCommand());
