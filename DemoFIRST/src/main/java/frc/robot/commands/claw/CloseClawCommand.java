8b5c6a4ecc84e7357329b865e99f7f56fc7e9e26
GrantPerkins <gcperk20@gmail.com>
2020-12-10 20:39:59 -0500

    Claw commands

---
+++ DemoFIRST/src/main/java/frc/robot/commands/claw/CloseClawCommand.java
@@ -0,0 +1,24 @@
+package frc.robot.commands.claw;
+
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.ClawSubsystem;
+
+
+public class CloseClawCommand extends WaitCommand {
+    private final ClawSubsystem claw;
+    public CloseClawCommand() {
+        super(Constants.ClawConstants.SERVO_ACTION_TIME);
+
+        this.claw = RobotContainer.clawSubsystem;
+
+        addRequirements(claw);
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        claw.open();
+    }
+}
